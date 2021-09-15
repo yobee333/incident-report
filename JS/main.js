d724c049f16259f14c9faef3ad7017076397aa5a
yobee333 <bridgetbrevik@outlook.com>
2021-09-15 08:47:36 -0500

    add injury area

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,8 @@
 
+document.querySelector('area').addEventListener('click', selectArea)
+
+function selectArea(){
+    const bodyPart = document.querySelector('area').value
+
+    document.querySelector('#selectedArea').innerText = bodyPart
+}
