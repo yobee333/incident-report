93edfdb3233a568a623ffc8afdc57b0f7eeea0ac
yobee333 <bridgetbrevik@outlook.com>
2021-09-22 11:25:19 -0500

    updated JS for duplicates

--- JS/main.js
+++ JS/main.js
@@ -7,25 +7,23 @@ document.getElementById('add-injury').addEventListener('click', addText)
 function addText() {
     const ul = document.getElementById('anatomy-list')
     const injuryLiList = ul.children
-    const injuryTextList = [...injuryLiList].map(li=> li.textContent)
+    const injuryTextList = [...injuryLiList].map(li=> li.id)
     console.log(injuryTextList)
 
     let injuryText = injuryInput.value
-    
+
     if(!injuryTextList.includes(injuryText)){
-        let node = document.createElement('li')
-        node.appendChild(document.createTextNode(injuryText))
-        
-        let button = document.createElement('button')
-        
-        button.textContent = '    X'
-        node.appendChild(button)
-        node.addEventListener('click', (event)=>{
-            const deleteButton = event.target;
-            const listItem = deleteButton.parentNode
-            listItem.remove()
-        })
-        document.querySelector('ul').appendChild(node)
+        ul.insertAdjacentHTML('beforeend', `
+            <li id="${injuryText}">
+                ${injuryText}
+                <button type="button" id="${injuryText}-btn">X</button>
+            </li>
+        `);
+        document.getElementById(`${injuryText}-btn`).addEventListener('click', (event)=>{
+                const deleteButton = event.target;
+                const listItem = deleteButton.parentNode
+                listItem.remove()
+            })
     }
 }
 
